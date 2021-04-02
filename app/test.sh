7ef7a2ff74c196652daf0817b28242d846d0da33
Felix <felix@aura.Speedport_W_724V_09011603_06_007>
2021-04-02 10:42:59 +0200

    Set coverage to 80%

--- app/test.sh
+++ app/test.sh
@@ -3,5 +3,5 @@ chmod +x coverage.json
 coverage json
 COV=$(cat coverage.json)
 PCT="$(echo "$COV" | python -c "import sys, json; print(json.load(sys.stdin)['totals']['percent_covered'])")"
-echo "$PCT" | python -c "import sys; import json; print('Test coverage is fine') if json.load(sys.stdin) > 99 else sys.exit(1)"
+echo "$PCT" | python -c "import sys; import json; print('Test coverage is fine') if json.load(sys.stdin) > 80 else sys.exit(1)"
 coverage report
