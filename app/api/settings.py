35ee46d85a2dc097fe65ab5eaeeac54d4d5de5a2
Felix Tepel <felix.tepel.extern@ebrain-solutions.de>
2021-11-21 14:55:54 +0000

    fix: fix email templates

--- app/api/settings.py
+++ app/api/settings.py
@@ -107,7 +107,7 @@ ROOT_URLCONF = 'api.urls'
 TEMPLATES = [
   {
     'BACKEND': 'django.template.backends.django.DjangoTemplates',
-    'DIRS': [BASE_DIR / 'api' / 'templates'],
+    'DIRS': [os.path.join(BASE_DIR, 'templates')],
     'APP_DIRS': True,
     'OPTIONS': {
       'context_processors': [
