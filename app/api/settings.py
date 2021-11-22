e44ed137bc6b81bb1f8e3215549d8ff5c6d9c885
Felix Tepel <felix.tepel.extern@ebrain-solutions.de>
2021-11-22 16:01:41 +0000

    feature: start verify email template

--- app/api/settings.py
+++ app/api/settings.py
@@ -110,7 +110,10 @@ ROOT_URLCONF = 'api.urls'
 TEMPLATES = [
   {
     'BACKEND': 'django.template.backends.django.DjangoTemplates',
-    'DIRS': [os.path.join(BASE_DIR, 'templates')],
+    'DIRS': [
+        os.path.join(BASE_DIR, 'templates'),
+        os.path.join(BASE_DIR, 'api', 'templates')
+    ],
     'APP_DIRS': True,
     'OPTIONS': {
       'context_processors': [
--- app/api/urls.py
+++ app/api/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
+from . import views
 
 urlpatterns = [
   path('api/admin/', admin.site.urls),
   path('api/users/', include('user.urls')),
   path('api/graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
+  path('api/verifyemail', views.verify_email)
 ]
